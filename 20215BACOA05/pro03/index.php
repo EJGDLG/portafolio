1b3877e78cf975439beb38db1bd24e9bfa3ea26d
EJGDLG <93065287+EJGDLG@users.noreply.github.com>
2024-05-24 22:15:17 -0600

    PHP

---
+++ 20215BACOA05/pro03/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Problema 3</title>
+	<meta charset="utf-8">
+	<link href="../../Recursos/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="../../Recursos/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+	<script src="../../Recursos/assets/js/ie-emulation-modes-warning.js"></script>
+</head>
+<body>
+	<center>
+		<h1>Problema  3</h1>
+		<p> Elabora un programa que convierta una cantidad de horas a minutos y segundos; al final muestra los resultados obtenidos</p>
+		<table>
+			<tr>
+				<td>
+					<form method="POST" name="FmPorcentaje" action="Resu03.php">
+						<table width="300" >
+							<tr>
+								<td width="65%"> cantidad de<br>Horas</td>
+								<td><input type="text" name="TxtH"></td>
+							</tr>
+							<tr>
+								<td colspan="2" align="center">
+								<input   class="btn btn-primary" type="submit" name="BtnEnviar" value="Enviar">
+								<input   class="btn btn-success" type="reset" name="BtnNuevo" value="Nuevo">
+								</td>
+							</tr>
+						</table>						
+					</form>
+				</td>
+			</tr>
+		</table>
+		<br>
+		<br>
+		<br>
+		<a class="btn btn-danger" href="../index.html">Regresar</a>	
+	</center>
+</body>
+</html>
